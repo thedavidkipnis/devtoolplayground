a862b5fe8734e2f87112c9ed4270f7e36b874da4
DK <thedavidkipnis@gmail.com>
2025-09-24 17:45:55 -0700

    Mobile fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,33 @@ import Navbar from './components/Navbar'
 import { useContent } from './context/ContentContext'
 
 function App() {
-
   const { displayedContent } = useContent();
 
   return (
     <div>
       <div className='content-wrapper'>
         <Navbar />
-        <div className='.content-display-wrapper'>
+        <div className='content-display-wrapper'>
           {displayedContent}
         </div>
 
-        <a className="portfolio-link" href='https://thedavidkipnis.github.io/portfolio2.0/' target="_blank" rel="noopener noreferrer">
-          {'>'} david kipnis, 2025
-        </a>
-
+        <div className='portfolio-link-wrapper'>
+          <a
+            className="portfolio-link"
+            href='https://thedavidkipnis.github.io/portfolio2.0/'
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {'>'} david kipnis, 2025
+          </a>
+        </div>
       </div>
-      <div className='mobile-support-banner'>mobile support is coming soon!</div>
+
+      <div className='mobile-support-banner'>
+        mobile support is coming soon!
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
